67b35f95be3893aacad69ed0476bdeb9c0c6b824
Atif Aziz <code@raboof.com>
2020-06-20 10:20:54 +0200

    Add compliance tests

---
+++ tests/Ast.cs
@@ -0,0 +1,456 @@
+namespace JmesPath.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    enum JsonValueKind { Null, Boolean, Number, String, Array, Object }
+
+    interface IJsonSystem<T>
+    {
+        T True { get; }
+        T False { get; }
+        T Null { get; }
+        JsonValueKind GetKind(T value);
+        T Parse(string json);
+        T String(string value);
+        bool GetMemberValue(T obj, string name, out T value);
+        bool GetBooleanValue(T value);
+        int GetLength(T value);
+    }
+
+    struct JsonValue
+    {
+        enum K : byte { Null, False, True, Number, String, Object, Array }
+
+        readonly K _kind;
+        readonly double _number;
+        readonly object _object;
+
+        JsonValue(K kind, double number = default, object obj = default) =>
+            (_kind, _number, _object) = (kind, number, obj);
+
+        public JsonValueKind Kind => _kind switch
+        {
+            K.Null   => JsonValueKind.Null,
+            K.False  => JsonValueKind.Boolean,
+            K.True   => JsonValueKind.Boolean,
+            K.Number => JsonValueKind.Number,
+            K.String => JsonValueKind.String,
+            K.Object => JsonValueKind.Object,
+            K.Array  => JsonValueKind.Array,
+        };
+
+        public static readonly JsonValue Null = new JsonValue(K.Null);
+        public static readonly JsonValue True = new JsonValue(K.True);
+        public static readonly JsonValue False = new JsonValue(K.False);
+
+        public static JsonValue Number(double num) => new JsonValue(K.Number, number: num);
+        public static JsonValue String(string str) => new JsonValue(K.String, obj: str);
+        public static JsonValue Array(IList<JsonValue> arr) => new JsonValue(K.Array, obj: arr);
+        public static JsonValue Object(IDictionary<string, JsonValue> obj) => new JsonValue(K.Object, obj: obj);
+
+        public static JsonValue Object(params (string, JsonValue)[] members)
+        {
+            var obj = new Dictionary<string, JsonValue>();
+            foreach (var (name, value) in members)
+                obj.Add(name, value);
+            return Object(obj);
+        }
+
+        public T Match<T>(T nul = default,
+                          Func<bool, T> bit = null,
+                          Func<double, T> num = null,
+                          Func<string, T> str = null,
+                          Func<IList<JsonValue>, T> arr = null,
+                          Func<IDictionary<string, JsonValue>, T> obj = null) =>
+            Match(() => nul, bit, num, str, arr, obj);
+
+        public T Match<T>(Func<T> nul = null,
+                          Func<bool, T> bit = null,
+                          Func<double, T> num = null,
+                          Func<string, T> str = null,
+                          Func<IList<JsonValue>, T> arr = null,
+                          Func<IDictionary<string, JsonValue>, T> obj = null) =>
+            _kind switch
+            {
+                K.Null   => nul is {} ? nul() : throw new InvalidOperationException(),
+                K.True   => bit is {} ? bit(true) : throw new InvalidOperationException(),
+                K.False  => bit is {} ? bit(false) : throw new InvalidOperationException(),
+                K.Number => num is {} ? num(_number) : throw new InvalidOperationException(),
+                K.String => str is {} ? str((string)_object) : throw new InvalidOperationException(),
+                K.Array  => arr is {} ? arr((IList<JsonValue>)_object) : throw new InvalidOperationException(),
+                K.Object => obj is {} ? obj((IDictionary<string, JsonValue>)_object) : throw new InvalidOperationException(),
+            };
+
+        public T Match<A, T>(A arg,
+                             Func<A, T> nul = null,
+                             Func<A, bool, T> bit = null,
+                             Func<A, double, T> num = null,
+                             Func<A, string, T> str = null,
+                             Func<A, IList<JsonValue>, T> arr = null,
+                             Func<A, IDictionary<string, JsonValue>, T> obj = null) =>
+            _kind switch
+            {
+                K.Null   => nul is {} ? nul(arg) : throw new InvalidOperationException(),
+                K.True   => bit is {} ? bit(arg, true) : throw new InvalidOperationException(),
+                K.False  => bit is {} ? bit(arg, false) : throw new InvalidOperationException(),
+                K.Number => num is {} ? num(arg, _number) : throw new InvalidOperationException(),
+                K.String => str is {} ? str(arg, (string)_object) : throw new InvalidOperationException(),
+                K.Array  => arr is {} ? arr(arg, (IList<JsonValue>)_object) : throw new InvalidOperationException(),
+                K.Object => obj is {} ? obj(arg, (IDictionary<string, JsonValue>)_object) : throw new InvalidOperationException(),
+            };
+    }
+
+    static class JsonSystem
+    {
+        public static readonly IJsonSystem<JsonValue> Default = new DefaultJsonSystem();
+
+        sealed class DefaultJsonSystem : IJsonSystem<JsonValue>
+        {
+            public JsonValue True => JsonValue.True;
+            public JsonValue False => JsonValue.False;
+            public JsonValue Null => JsonValue.Null;
+
+            public JsonValueKind GetKind(JsonValue value) => value.Kind;
+
+            public JsonValue Parse(string json)
+            {
+                throw new NotImplementedException();
+            }
+
+            public JsonValue String(string value) => JsonValue.String(value);
+
+            public bool GetMemberValue(JsonValue obj, string name, out JsonValue value)
+            {
+                if (obj.Kind != JsonValueKind.Object)
+                    throw new ArgumentOutOfRangeException(nameof(obj), obj, null);
+                if (obj.Match(name, obj: (n, obj) => obj.TryGetValue(n, out var v) ? v : (JsonValue?)null) is {} v)
+                {
+                    value = v;
+                    return true;
+                }
+                else
+                {
+                    value = default;
+                    return false;
+                }
+            }
+
+            public bool GetBooleanValue(JsonValue value) =>
+                value.Match(nul: (bool?)null,
+                            bit: bit => bit,
+                            num: _ => null,
+                            str: _ => null,
+                            arr: _ => null,
+                            obj: _ => null)
+                    is {} bit ? bit : throw new ArgumentOutOfRangeException(nameof(value), value, null);
+
+            public int GetLength(JsonValue value) =>
+                value.Match(nul: (int?)null,
+                            bit: _ => null,
+                            num: _ => null,
+                            str: str => str.Length,
+                            arr: arr => arr.Count,
+                            obj: obj => obj.Count)
+                    is {} len ? len : throw new ArgumentOutOfRangeException(nameof(value), value, null);
+        }
+
+        public static bool IsTruthy<T>(this IJsonSystem<T> system, T value)
+        {
+            // A false value corresponds to any of the following conditions:
+            //
+            // - Empty list   : []
+            // - Empty object : {}
+            // - Empty string : ""
+            // - False boolean: false
+            // - Null value   : null
+            //
+            // A true value corresponds to any value that is not false.
+
+            switch (system.GetKind(value))
+            {
+                case JsonValueKind.Number:
+                case JsonValueKind.Null:
+                    return false;
+                case JsonValueKind.Boolean:
+                    return system.GetBooleanValue(value);
+                case JsonValueKind.String:
+                case JsonValueKind.Array:
+                case JsonValueKind.Object:
+                    return system.GetLength(value) > 0;
+                default:
+                    throw new Exception("Implementation error.");
+            }
+        }
+    }
+
+    abstract class Node
+    {
+        public abstract T Evaluate<T>(T value, IJsonSystem<T> system);
+    }
+
+    sealed class IdentityNode : Node
+    {
+        public static readonly IdentityNode Value = new IdentityNode();
+
+        public override T Evaluate<T>(T value, IJsonSystem<T> system) => value;
+    }
+
+    sealed class StringNode : Node
+    {
+        public string Value { get; }
+
+        public StringNode(string value) =>
+            Value = value;
+
+        public override T Evaluate<T>(T value, IJsonSystem<T> system) =>
+            system.String(Value);
+
+        public override string ToString() => Value;
+
+        public static implicit operator string(StringNode node) => node.Value;
+    }
+
+    sealed class LiteralNode : Node
+    {
+        public string Json { get; }
+
+        public LiteralNode(string json) =>
+            Json = json;
+
+        public override T Evaluate<T>(T value, IJsonSystem<T> system) =>
+            system.Parse(Json);
+
+        public override string ToString() => Json;
+    }
+
+    sealed class FieldNode : Node
+    {
+        public string Name { get; }
+
+        public FieldNode(string name) => Name = name;
+
+        public override T Evaluate<T>(T value, IJsonSystem<T> system)
+            => system.GetKind(value) == JsonValueKind.Object
+            && system.GetMemberValue(value, Name, out var v) ? v : system.Null;
+    }
+
+    sealed class NotNode : Node
+    {
+        public Node Expression { get; }
+
+        public NotNode(Node expression) => Expression = expression;
+
+        public override T Evaluate<T>(T value, IJsonSystem<T> system)
+            => system.IsTruthy(Expression.Evaluate(value, system)) ? system.False : system.True;
+    }
+
+    abstract class BinaryNode : Node
+    {
+        public Node Left { get; }
+        public Node Right { get; }
+
+        public BinaryNode(Node left, Node right) =>
+            (Left, Right) = (left, right);
+    }
+
+    sealed class AndNode : BinaryNode
+    {
+        public AndNode(Node left, Node right) : base(left, right) {}
+
+        public override T Evaluate<T>(T value, IJsonSystem<T> system)
+            => system.IsTruthy(Left.Evaluate(value, system))
+            && system.IsTruthy(Right.Evaluate(value, system)) ? system.True : system.False;
+    }
+
+    sealed class OrNode : BinaryNode
+    {
+        public OrNode(Node left, Node right) : base(left, right) { }
+
+        public override T Evaluate<T>(T value, IJsonSystem<T> system)
+            => system.IsTruthy(Left.Evaluate(value, system))
+            && system.IsTruthy(Right.Evaluate(value, system)) ? system.True : system.False;
+    }
+
+    sealed class TreeProjector : ITreeProjector<Node>
+    {
+        public Node RawString(string s, int index, int length)
+        {
+            return new StringNode(Unescape(s.Substring(index + 1, length - 2)));
+
+            static string Unescape(string s)
+            {
+                StringBuilder sb = null;
+                for (var si = 0; ;)
+                {
+                    var i = s.IndexOf('\\', si);
+                    if (i < 0)
+                        break;
+                    sb ??= new StringBuilder();
+                    sb.Append(s, si, i - si).Append(s[i + 1]);
+                    si = i + 2;
+                }
+                return sb?.ToString() ?? s;
+            }
+        }
+
+        public Node UnquotedString(string s, int index, int length) =>
+            new StringNode(s.Substring(index, length));
+
+        public Node QuotedString(string s, int index, int length)
+        {
+            return new StringNode(Unescape(s.Substring(index + 1, length - 2)));
+
+            static string Unescape(string s)
+            {
+                StringBuilder sb = null;
+                for (var si = 0; ;)
+                {
+                    var i = s.IndexOf('\\', si);
+                    if (i < 0)
+                        break;
+                    var replacement = s[i + 1] switch
+                    {
+                        '\\' => '\\',
+                        '"' => '"',
+                        '/' => '/',
+                        'b' => '\b',
+                        'f' => '\f',
+                        'n' => '\n',
+                        'r' => '\r',
+                        'v' => '\v',
+                        't' => '\t',
+                        _ => (char?)null
+                    };
+                    sb ??= new StringBuilder();
+                    sb.Append(s, si, i - si);
+                    si = i + 1;
+                    if (replacement is {} ch)
+                    {
+                        sb.Append(ch);
+                        si++;
+                    }
+                }
+                return sb?.ToString() ?? s;
+            }
+        }
+
+        public Node Literal(string s, int index, int length) =>
+            new LiteralNode(s.Substring(index, length));
+
+        public Node CurrentNode()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Node Not(Node expression)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Node And(Node left, Node right)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Node Or(Node left, Node right)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Node GreaterThan(Node left, Node right)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Node GreaterThanEqual(Node left, Node right)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Node LessThan(Node left, Node right)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Node LessThanEqual(Node left, Node right)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Node Equal(Node left, Node right)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Node NotEqual(Node left, Node right)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Node Flatten(Node source)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Node Field(Node name) => new FieldNode((StringNode)name);
+
+        public Node Identity() => IdentityNode.Value;
+
+        public Node ValueProjection(Node left, Node right)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Node MultiSelectList(Node[] list)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Node MultiSelectHash(KeyValuePair<string, Node>[] hash)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Node FilterProjection(Node left, Node right, Node condition)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Node Projection(Node left, Node right)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Node Index(Node source, int index)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Node Slice(int? start, int? stop, int? step)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Node Reference()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Node SubExpression(Node left, Node right)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Node Pipe(Node left, Node right)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Function(Node name, Node[] args)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ tests/ComplianceTests.cs
@@ -0,0 +1,90 @@
+namespace JmesPath.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text.Json;
+    using NUnit.Framework;
+    using NUnit.Framework.Interfaces;
+
+    public class ComplianceTests
+    {
+        [TestCaseSource(nameof(TestData), new object[] { true })]
+        public void Result(JsonElement given, string expression, JsonElement result)
+        {
+        }
+
+        [TestCaseSource(nameof(TestData), new object[] { false })]
+        public void Error(JsonElement given, string expression, string error)
+        {
+            //var pe = Expression.Parse(expression);
+            //var projector = new TreeProjector<Node>(new TreeProjector());
+            //Machine.Run(pe, projector);
+            //projector.Result.Evaluate();
+        }
+
+        static IEnumerable<ITestCaseData> TestData(bool success) =>
+            from t in ComplianceTestData
+            let re = t.Match(r => new { Result = r, Error = (string)null },
+                             e => new { Result = default(JsonElement), Error = e })
+            where !success && re.Error is {}
+               || success && re.Error is null
+            select new TestCaseData(t.Given, t.Expression, re.Error ?? (object)re.Result)
+                .SetName($"{(success ? "Result" : "Error")}.{t.Name}({t.Expression}) << {t.Given})");
+
+        abstract class ComplianceRecord
+        {
+            public readonly string Name;
+            public readonly JsonElement Given;
+            public readonly string Expression;
+
+            public static ComplianceRecord
+                Result(string name, JsonElement given, string expression, JsonElement result) =>
+                new SuccessRecord(name, given, expression, result);
+
+            public static ComplianceRecord
+                Error(string name, JsonElement given, string expression, string error) =>
+                new ErrorRecord(name, given, expression, error);
+
+            ComplianceRecord(string name, JsonElement given, string expression) =>
+                (Name, Given, Expression) = (name, given, expression);
+
+            public abstract T Match<T>(Func<JsonElement, T> result, Func<string, T> error);
+
+            sealed class SuccessRecord : ComplianceRecord
+            {
+                readonly JsonElement _result;
+                
+                public SuccessRecord(string name, JsonElement given, string expression, JsonElement result) :
+                    base(name, given, expression) => _result = result;
+
+                public override T Match<T>(Func<JsonElement, T> result,
+                                           Func<string, T> error) => result(_result);
+            }
+
+            sealed class ErrorRecord : ComplianceRecord
+            {
+                readonly string _error;
+
+                public ErrorRecord(string name, JsonElement given, string expression, string error) :
+                    base(name, given, expression) => _error = error;
+
+                public override T Match<T>(Func<JsonElement, T> result,
+                                           Func<string, T> error) => error(_error);
+            }
+        }
+
+        static IEnumerable<ComplianceRecord> ComplianceTestData =>
+            from path in Directory.EnumerateFiles(Path.Join(TestContext.CurrentContext.TestDirectory, "tests"), "*.json")
+            let source = Path.GetFileNameWithoutExtension(path)
+            from t in JsonDocument.Parse(File.ReadAllText(path)).RootElement.EnumerateArray()
+            let given = t.GetProperty("given")
+            from c in t.GetProperty("cases").EnumerateArray()
+            let e = c.GetProperty("expression").GetString()
+            where !c.TryGetProperty("bench", out _)
+            select c.TryGetProperty("result", out var res)
+                 ? ComplianceRecord.Result(source, given, e, res)
+                 : ComplianceRecord.Error(source, given, e, c.GetProperty("error").GetString());
+    }
+}
